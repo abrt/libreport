85fb1dfc917fcea35b0cc159ad279aabef2938d7
Michal Toman <mtoman@redhat.com>
2011-08-19 11:43:35 +0200

    use a fallback text editor if xdg-open fails

--- src/gui-wizard-gtk/wizard.c
+++ src/gui-wizard-gtk/wizard.c
@@ -534,6 +534,7 @@ static void tv_details_row_activated(
     if (!strchr(item->content, '\n')) /* one line? */
         goto ret; /* yes */
 
+    gint exitcode;
     gchar *arg[3];
     arg[0] = (char *) "xdg-open";
     arg[1] = concat_path_file(g_dump_dir_name, item_name);
@@ -541,7 +542,37 @@ static void tv_details_row_activated(
 
     g_spawn_sync(NULL, arg, NULL,
                  G_SPAWN_SEARCH_PATH | G_SPAWN_STDOUT_TO_DEV_NULL,
-                 NULL, NULL, NULL, NULL, NULL, NULL);
+                 NULL, NULL, NULL, NULL, &exitcode, NULL);
+
+    if (exitcode != EXIT_SUCCESS)
+    {
+        GtkWidget *dialog = gtk_dialog_new_with_buttons(_("View/edit a text file"),
+            GTK_WINDOW(g_assistant),
+            GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+            NULL);
+        GtkWidget *vbox = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+        GtkWidget *scrolled = gtk_scrolled_window_new(NULL, NULL);
+        GtkWidget *textview = gtk_text_view_new();
+
+        gtk_dialog_add_button(GTK_DIALOG(dialog), GTK_STOCK_SAVE, GTK_RESPONSE_OK);
+        gtk_dialog_add_button(GTK_DIALOG(dialog), GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
+
+        gtk_box_pack_start(GTK_BOX(vbox), scrolled, TRUE, TRUE, 0);
+        gtk_widget_set_size_request(scrolled, 640, 480);
+        gtk_widget_show(scrolled);
+
+        gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(scrolled), textview);
+        gtk_widget_show(textview);
+
+        load_text_to_text_view(GTK_TEXT_VIEW(textview), item_name);
+
+        if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_OK)
+            save_text_from_text_view(GTK_TEXT_VIEW(textview), item_name);
+
+        gtk_widget_destroy(textview);
+        gtk_widget_destroy(scrolled);
+        gtk_widget_destroy(dialog);
+    }
 
     free(arg[1]);
  ret:
