e2a39388c6e90906a0b9f9d06b2bcfd2535a22a5
Denys Vlasenko <dvlasenk@redhat.com>
2012-01-11 18:08:29 +0100

    reporter-bugzilla: mention -f in help text
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>

--- src/plugins/reporter-bugzilla.c
+++ src/plugins/reporter-bugzilla.c
@@ -40,7 +40,7 @@ int main(int argc, char **argv)
     /* Can't keep these strings/structs static: _() doesn't support that */
     const char *program_usage_string = _(
         "\n"
-        "& [-vb] [-c CONFFILE] -d DIR\n"
+        "& [-vbf] [-c CONFFILE] -d DIR\n"
         "or:\n"
         "& [-v] [-c CONFFILE] [-d DIR] -t[ID] FILE...\n"
         "\n"
