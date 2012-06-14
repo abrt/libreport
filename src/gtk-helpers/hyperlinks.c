69e2197db09dab73459730a5aa9e4407ec41d8d0
Jiri Moskovcak <jmoskovc@redhat.com>
2012-06-14 13:57:41 +0200

    fixed hyperlink parsing rhbz#831333

--- src/gtk-helpers/hyperlinks.c
+++ src/gtk-helpers/hyperlinks.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 #include <internal_libreport_gtk.h>
 
+
+static char *find_end(const char* url_start)
+{
+    static const char *known_url_ends = ",'\"\t\n ";
+
+    char *url_end = strchrnul(url_start, *known_url_ends);
+    const char *curr_end = known_url_ends;
+    while(*(++curr_end) != '\0')
+    {
+        char *tmp = strchrnul(url_start, *curr_end);
+        if (tmp < url_end)
+            url_end = tmp;
+    }
+    return url_end;
+}
+
 char *tag_url(const char* line, const char* prefix)
 {
     static const char *const known_url_prefixes[] = {"http://", "https://", "ftp://", "file://", NULL};
@@ -16,7 +32,7 @@ char *tag_url(const char* line, const char* prefix)
         char *url_start;
         while ((url_start = strstr(cur_pos, *pfx)) != NULL)
         {
-            char *url_end = strchrnul(url_start, ' '); //TODO: also '.', ',', '\t', '\n'...
+            char *url_end = find_end(url_start);
             int len = url_end - url_start;
             char *hyperlink = xasprintf("%s<a href=\"%.*s\">%.*s</a>",
                             prefix,
