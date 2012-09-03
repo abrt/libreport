eb20ae346b25741100078673ebb56da29673023a
Jakub Filak <jfilak@redhat.com>
2012-09-03 16:56:22 +0200

    trac#547: remove forgotten text from report-gtk usage string
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- src/gui-wizard-gtk/main.c
+++ src/gui-wizard-gtk/main.c
@@ -104,7 +104,7 @@ int main(int argc, char **argv)
 
     /* Can't keep these strings/structs static: _() doesn't support that */
     const char *program_usage_string = _(
-        "& [-vpd] [-g GUI_FILE] [-n PROG_NAME] DIR\n"
+        "& [-vpd] [-g GUI_FILE] DIR\n"
         "\n"
         "GUI tool to analyze and report problem saved in specified DIR"
     );
