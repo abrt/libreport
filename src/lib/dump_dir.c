36f142f2ee30f5990983a37ac41f3eba6f29d681
Denys Vlasenko <vda.linux@googlemail.com>
2013-01-16 12:43:13 +0100

    make dd_delete_item check that dd is locked
    Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

--- src/lib/dump_dir.c
+++ src/lib/dump_dir.c
@@ -850,13 +850,16 @@ long dd_get_item_size(struct dump_dir *dd, const char *name)
 
 int dd_delete_item(struct dump_dir *dd, const char *name)
 {
+    if (!dd->locked)
+        error_msg_and_die("dump_dir is not opened"); /* bug */
+
     char *path = concat_path_file(dd->dd_dirname, name);
     int res = unlink(path);
 
     if (res < 0)
     {
         if (errno == ENOENT)
-            res = 0;
+            errno = res = 0;
         else
             perror_msg("Can't delete file '%s'", path);
     }
