4ed4e39285da4a8af1f40fbc403a32469b8371b0
Denys Vlasenko <dvlasenk@redhat.com>
2013-03-27 13:03:02 +0100

    reporter-upload: move file upload function to lib/
    This patch moves send_file() out of reporter-upload
    into lib/curl.c and renames it to upload_file().
    It returns new URL or NULL on error.
    
    Signed-off-by: Denys Vlasenko <dvlasenk@redhat.com>
    Signed-off-by: Jiri Moskovcak <jmoskovc@redhat.com>

--- src/lib/abrt_curl.h
+++ src/lib/abrt_curl.h
@@ -117,6 +117,9 @@ abrt_post_file_as_form(abrt_post_state_t *state,
                      filename, ABRT_POST_DATA_FROMFILE_AS_FORM_DATA);
 }
 
+#define upload_file libreport_upload_file
+char *upload_file(const char *url, const char *filename);
+
 #ifdef __cplusplus
 }
 #endif
