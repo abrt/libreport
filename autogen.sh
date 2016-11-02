c9687493bc9dd0db46803973cb3304dc981cf5bc
Jakub Filak <jfilak@redhat.com>
2016-11-02 12:20:36 +0100

    build: configure tree for debugging by default
    Make hacker's life easier.
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -68,6 +68,18 @@ case "$1" in
             automake --add-missing --force --copy || exit 1
 
             echo "Running configure ..."
-            ./configure "$@"
+            if [ 0 -eq $# ]; then
+                ./configure \
+                    --prefix=/usr \
+                    --sysconfdir=/etc \
+                    --localstatedir=/var \
+                    --sharedstatedir=/var/lib \
+                    --mandir=/usr/share/man \
+                    --infodir=/usr/share/info \
+                    --enable-debug
+                echo "Configured for local debugging ..."
+            else
+                ./configure "$@"
+            fi
         ;;
 esac
