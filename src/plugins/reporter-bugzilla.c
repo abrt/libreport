abrt/libreport

commit 946c522ba702c11b7b336552461747541e714bb7

reporter-bugzilla: use /etc/os-release for default url
Defaulting to the value set in /etc/os-release allows for easier
integration by respins.

Closes #459.

Signed-off-by: Jakub Filak <jakub@thefilaks.net>

src/plugins/reporter-bugzilla.c
@@ -86,6 +86,27 @@ struct bugzilla_struct {
     GList       *b_private_groups;
 };
 
+static void set_default_settings(map_string_t *osinfo, map_string_t *settings)
+{
+    char *default_BugzillaURL;
+    parse_osinfo_for_bug_url(osinfo, &default_BugzillaURL);
+    /* if BugzillaURL is defined in conf_file or env , it will replace this value */
+    set_map_string_item_from_string(settings, "BugzillaURL", default_BugzillaURL);
+    log_debug("Loaded BUG_REPORT_URL '%s' from os-release", default_BugzillaURL);
+    free(default_BugzillaURL);
+
+    char *default_Product;
+    char *default_ProductVersion;
+    parse_osinfo_for_bz(osinfo, &default_Product, &default_ProductVersion);
+    /* if Product or ProductVersion is defined in conf_file or env , it will replace this value */
+    set_map_string_item_from_string(settings, "Product", default_Product);
+    set_map_string_item_from_string(settings, "ProductVersion", default_ProductVersion);
+    log_debug("Loaded Product '%s' from os-release", default_Product);
+    log_debug("Loaded ProductVersion '%s' from os-release", default_ProductVersion);
+    free(default_Product);
+    free(default_ProductVersion);
+}
+
 static void set_settings(struct bugzilla_struct *b, map_string_t *settings)
 {
     const char *environ;
@@ -97,7 +118,7 @@ static void set_settings(struct bugzilla_struct *b, map_string_t *settings)
     b->b_password = xstrdup(environ ? environ : get_map_string_item_or_empty(settings, "Password"));
 
     environ = getenv("Bugzilla_BugzillaURL");
-    b->b_bugzilla_url = environ ? environ : get_map_string_item_or_empty(settings, "BugzillaURL");
+    b->b_bugzilla_url = xstrdup(environ ? environ : get_map_string_item_or_empty(settings, "BugzillaURL"));
     if (!b->b_bugzilla_url[0])
         b->b_bugzilla_url = "https://bugzilla.redhat.com";
     else
@@ -313,6 +334,19 @@ int main(int argc, char **argv)
     export_abrt_envvars(0);
 
     map_string_t *settings = new_map_string();
+    problem_data_t *problem_data;
+
+    /* pull in some defaults from os-release */
+    problem_data = create_problem_data_for_reporting(dump_dir_name);
+    if (!problem_data)
+        xfunc_die(); /* create_problem_data_for_reporting already emitted error msg */
+    else
+    {
+        map_string_t *osinfo = new_map_string();
+        problem_data_get_osinfo(problem_data, osinfo);
+        set_default_settings(osinfo, settings);
+        free_map_string(osinfo);
+    }
 
     {
         if (!conf_file)
@@ -519,10 +553,6 @@ int main(int argc, char **argv)
         }
         free_report_result(reported_to);
     }
-
-    problem_data_t *problem_data = create_problem_data_for_reporting(dump_dir_name);
-    if (!problem_data)
-        xfunc_die(); /* create_problem_data_for_reporting already emitted error msg */
 
     const char *component = problem_data_get_content_or_die(problem_data, FILENAME_COMPONENT);
     const char *duphash   = problem_data_get_content_or_NULL(problem_data, FILENAME_DUPHASH);

src/include/internal_libreport.h
@@ -859,6 +859,20 @@ void parse_osinfo(const char *osinfo_bytes, map_string_t *osinfo);
 #define parse_osinfo_for_bz libreport_parse_osinfo_for_bz
 void parse_osinfo_for_bz(map_string_t *osinfo, char **product, char **version);
 
+/* @brief Extract BUG_REPORT_URL from os-release
+ *
+ * A default location for bug reports can be stored in os-release.
+ * This extracts the value if present and stores it in url.
+ * If unset, url will become NULL
+ *
+ * https://github.com/abrt/libreport/issues/459
+ *
+ * @param osinfo Input data from which the values are built
+ * @param url Non-NULL pointer where pointer to malloced string will be stored. Memory must be released by free()
+ */
+#define parse_osinfo_for_bug_url libreport_parse_osinfo_for_bug_url
+void parse_osinfo_for_bug_url(map_string_t *osinfo, char** url);
+
 /* @brief Builds product string and product's version string for Red Hat Support
  *
  * At first tries to get strings from the os specific variables

src/lib/parse_release.c
@@ -289,6 +289,19 @@ void parse_osinfo_for_bz(map_string_t *osinfo, char** product, char** version)
     *version = NULL;
 }
 
+void parse_osinfo_for_bug_url(map_string_t *osinfo, char** url)
+{
+    const char *os_bug_report_url = get_map_string_item_or_NULL(osinfo, "BUG_REPORT_URL");
+
+    if (os_bug_report_url)
+        *url = xstrdup(os_bug_report_url);
+    else
+        /* unset or something bad happend */
+        *url = NULL;
+
+    return;
+}
+
 /*
  * Note to developers: you can use
  * curl --silent -u $USER:$PASS "https://api.access.redhat.com/rs/products/Red%20Hat%20Enterprise%20Linux/versions" | xmllint --format -

src/plugins/bugzilla.conf
@@ -1,5 +1,5 @@
-# Bugzilla URL
-BugzillaURL = https://bugzilla.redhat.com/
+# Bugzilla URL, defaults to BUG_REPORT_URL from /etc/os-release
+# BugzillaURL = https://bugzilla.example.com/
 # yes means that ssl certificates will be checked
 SSLVerify = yes
 # your login has to exist, if you don't have any, please create one

tests/osinfo.at
@@ -213,6 +213,73 @@ int main(void)
 }
 ]])
 
+## ----------------------- ##
+## parse_osinfo_for_bugurl ##
+## ----------------------- ##
+
+AT_TESTFUN([parse_osinfo_for_bugurl], [[
+#include "testsuite.h"
+#include "internal_libreport.h"
+#include <assert.h>
+
+void test(const char *osinfo_bytes, const char *url)
+{
+    map_string_t *osinfo = new_map_string();
+    parse_osinfo(osinfo_bytes, osinfo);
+
+    char *default_BugzillaURL = (char *)0xDEADBEEF;
+    parse_osinfo_for_bug_url(osinfo, &default_BugzillaURL);
+
+    TS_ASSERT_STRING_EQ(default_BugzillaURL, url, "Tested URL");
+
+    free(default_BugzillaURL);
+    free_map_string(osinfo);
+}
+
+int main(void)
+{
+    g_verbose=3;
+
+    test("NAME=Fedora\n"
+         "VERSION=\"19 (Schrödinger\\'s Cat)\"\n"
+         "ID=fedora\n"
+         "VERSION_ID=19\n"
+         "PRETTY_NAME=\"Fedora 19 (Schrödinger\\'s Cat)\"\n"
+         "ANSI_COLOR=\"0;34\"\n"
+         "CPE_NAME=\"cpe:/o:fedoraproject:fedora:18\"\n"
+         ,NULL);
+
+    test("NAME=\"Red Hat Enterprise Linux Client\"\n"
+         "VERSION=\"7.0 Beta (Maipo)\"\n"
+         "ID=rhel\n"
+         "VERSION_ID=7.0\n"
+         "PRETTY_NAME=\"Red Hat Enterprise Linux Client 7.0 Beta (Maipo)\"\n"
+         "ANSI_COLOR=\"0;31\"\n"
+         "CPE_NAME=\"cpe:/o:redhat:enterprise_linux:7.0.0:beta:client\"\n"
+         "BUG_REPORT_URL=\"notrfc\"\n"
+         "REDHAT_BUGZILLA_PRODUCT=\"Red Hat Enterprise Linux 7\"\n"
+         "REDHAT_BUGZILLA_PRODUCT_VERSION=7.1\n"
+         ,"notrfc");
+
+    test("NAME=\"Red Hat Enterprise Linux Client\"\n"
+         "VERSION=\"7.0 Beta (Maipo)\"\n"
+         "ID=rhel\n"
+         "VERSION_ID=7.0\n"
+         "PRETTY_NAME=\"Red Hat Enterprise Linux Client 7.0 Beta (Maipo)\"\n"
+         "ANSI_COLOR=\"0;31\"\n"
+         "CPE_NAME=\"cpe:/o:redhat:enterprise_linux:7.0.0:beta:client\"\n"
+         "BUG_REPORT_URL=\"http://bugzilla.redhat.com\"\n"
+         "REDHAT_BUGZILLA_PRODUCT=\"Red Hat Enterprise Linux 7\"\n"
+         "REDHAT_BUGZILLA_PRODUCT_VERSION=7.1\n"
+         ,"http://bugzilla.redhat.com");
+
+    test("" , NULL);
+
+    return 0;
+}
+]])
+
+
 ## --------------------- ##
 ## parse_osinfo_for_rhts ##
 ## --------------------- ##

commit cfff49f06d9d17e269d463ee71b3d94c8bfb0c58

bugzilla: add check if option -d was entered
The change from commit 946c522ba702c11b7b336552461747541e714bb7 caused that reporter was ALWAYS trying to read
from problem directory. And because the default value for it is set to ".",
running reporter from outside of problem directory caused errors.

Signed-off-by: Martin Kutlak <mkutlak@redhat.com>

src/plugins/reporter-bugzilla.c
@@ -334,18 +334,21 @@ int main(int argc, char **argv)
     export_abrt_envvars(0);
 
     map_string_t *settings = new_map_string();
-    problem_data_t *problem_data;
+    problem_data_t *problem_data = NULL;
 
-    /* pull in some defaults from os-release */
-    problem_data = create_problem_data_for_reporting(dump_dir_name);
-    if (!problem_data)
-        xfunc_die(); /* create_problem_data_for_reporting already emitted error msg */
-    else
+    if (opts & OPT_d)
     {
-        map_string_t *osinfo = new_map_string();
-        problem_data_get_osinfo(problem_data, osinfo);
-        set_default_settings(osinfo, settings);
-        free_map_string(osinfo);
+        /* pull in some defaults from os-release */
+        problem_data = create_problem_data_for_reporting(dump_dir_name);
+        if (!problem_data)
+            xfunc_die(); /* create_problem_data_for_reporting already emitted error msg */
+        else
+        {
+            map_string_t *osinfo = new_map_string();
+            problem_data_get_osinfo(problem_data, osinfo);
+            set_default_settings(osinfo, settings);
+            free_map_string(osinfo);
+        }
     }
 
     {
@@ -553,6 +556,12 @@ int main(int argc, char **argv)
         }
         free_report_result(reported_to);
     }
+
+    if (!(opts & OPT_d))
+        problem_data = create_problem_data_for_reporting(dump_dir_name);
+
+    if (!problem_data)
+        xfunc_die(); /* create_problem_data_for_reporting already emitted error msg */
 
     const char *component = problem_data_get_content_or_die(problem_data, FILENAME_COMPONENT);
     const char *duphash   = problem_data_get_content_or_NULL(problem_data, FILENAME_DUPHASH);