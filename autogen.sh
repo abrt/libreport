a5c7c79c5e7346f0c11c3fb82b21e210ef62b9d8
Ernestas Kulik <ekulik@redhat.com>
2019-02-04 12:31:19 +0100

    autogen.sh: Use autoreconf
    autoreconf replaces the need to run autoconf, autoheader, aclocal,
    automake, autopoint and libtoolize manually.
    
    Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -54,30 +54,11 @@ case "$1" in
             echo "Running gen-version"
             ./gen-version
 
-            mkdir -p m4
-            echo "Creating m4/aclocal.m4 ..."
-            test -r m4/aclocal.m4 || touch m4/aclocal.m4
-
-            echo "Running autopoint"
-            autopoint --force || exit 1
+            echo "Running autoreconf"
+            autoreconf -ifv || exit 1
 
             echo "Running intltoolize..."
             intltoolize --force --copy --automake || exit 1
-
-            echo "Running aclocal..."
-            aclocal || exit 1
-
-            echo "Running libtoolize..."
-            libtoolize || exit 1
-
-            echo "Running autoheader..."
-            autoheader || return 1
-
-            echo "Running autoconf..."
-            autoconf --force || exit 1
-
-            echo "Running automake..."
-            automake --add-missing --force --copy || exit 1
 
             if [ "$NOCONFIGURE" = "" ]; then
                 echo "Running configure ..."
