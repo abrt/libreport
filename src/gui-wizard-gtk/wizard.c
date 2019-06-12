abrt/libreport

commit f733f67523baecc8aeec490aa380a91d060ac669

gui-wizard-gtk: Fix never-read assignment
Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

src/gui-wizard-gtk/wizard.c
@@ -2754,7 +2754,8 @@ static char *get_next_processed_event(GList **events_list)
 
         GList *expanded_list = NULL;
         /* add expanded events from event having trailing '*' */
-        char *next = event_name = expanded_events;
+        char *next;
+        char *event_name = expanded_events;
         while ((next = strchr(event_name, '\n')))
         {
             /* 'event1\0event2\nevent3\n' */

commit 1ad60af31f4f899186ed444c5a4eab1389859f81

gui-wizard-gtk: Fix fix
f733f67523baecc8aeec490aa380a91d060ac669 mistakenly adds a redefinition
of event_name.

Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

src/gui-wizard-gtk/wizard.c
@@ -2755,7 +2755,7 @@ static char *get_next_processed_event(GList **events_list)
         GList *expanded_list = NULL;
         /* add expanded events from event having trailing '*' */
         char *next;
-        char *event_name = expanded_events;
+        event_name = expanded_events;
         while ((next = strchr(event_name, '\n')))
         {
             /* 'event1\0event2\nevent3\n' */