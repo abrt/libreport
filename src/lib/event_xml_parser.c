abrt/libreport

commit cf1bd4eb2ff931c0e9fb50d1df21099d70ca7926

xml parser: be more verbose in case xml file cannot be opened
In a case a file cannot be openned by function
load_event_description_from_file(), no error messages was printed
and there is no other way to realize that the operation failed.

Signed-off-by: Matej Habrnal <mhabrnal@redhat.com>

src/lib/event_xml_parser.c
@@ -577,6 +577,8 @@ void load_event_description_from_file(event_config_t *event_config, const char* 
         }
         fclose(fin);
     }
+    else
+        perror_msg("cannot load event description from file : '%s'", filename);
 
     g_markup_parse_context_free(context);
 

commit 2acef43a2ba415727d686ab81586c3de10364ab5

lib: Reintroduce error logging in event XML parser
This logging point was introduced in cf1bd4e and reverted in 74a85e6 with the
intention of bringing it back once wildcard expansions are fixed.

src/lib/event_xml_parser.c
@@ -546,11 +546,15 @@ static void error(GMarkupParseContext *context,
  * event_config_t contains list of options, which is malloced by hits function
  * and must be freed by the caller
  */
-
 void load_event_description_from_file(event_config_t *event_config, const char* filename)
 {
     log_info("loading event: '%s'", filename);
-    struct my_parse_data parse_data = { {event_config, false, false, false}, {NULL, false, false}, NULL, NULL };
+    struct my_parse_data parse_data = {
+        { event_config, false, false, false },
+        { NULL, false, false },
+        NULL,
+        NULL
+    };
     parse_data.cur_locale = xstrdup(setlocale(LC_ALL, NULL));
     strchrnul(parse_data.cur_locale, '.')[0] = '\0';
 
@@ -577,6 +581,8 @@ void load_event_description_from_file(event_config_t *event_config, const char* 
         }
         fclose(fin);
     }
+    else
+        perror_msg("cannot load event description from file: '%s'", filename);
 
     g_markup_parse_context_free(context);
 