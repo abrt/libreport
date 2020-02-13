a366009697f69d6080e0286fe5532bdb6b21e942
Ernestas Kulik <ekulik@redhat.com>
2020-02-13 08:45:29 +0100

    plugins: report: Use “rhts” instead of “strata”
    No need for this to be so specific, since it’s only targeting Red Hat
    anyway.

--- src/plugins/report.c
+++ src/plugins/report.c
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
         "Uploads FILEs to specified ticket on TARGET.\n"
         "\n"
         "This tool is provided to ease transition of users of report package\n"
-        "to libreport. Recognized TARGETs are 'strata' and 'bugzilla',\n"
+        "to libreport. Recognized TARGETs are 'rhts' and 'bugzilla',\n"
         "first one invokes upload to RHTSupport and second - to Bugzilla.\n"
         "\n"
         "Configuration (such as login data) can be supplied via files\n"
@@ -56,7 +56,7 @@ int main(int argc, char **argv)
     /* Keep enum above and order of options below in sync! */
     struct options program_options[] = {
         OPT__VERBOSE(&g_verbose),
-        OPT_STRING('T', "target", &target, "TARGET", _("'strata' or 'bugzilla'")),
+        OPT_STRING('T', "target", &target, "TARGET", _("'rhts' or 'bugzilla'")),
         OPT_STRING('t', "ticket", &ticket, "ID"    , _("Ticket/case ID")),
         OPT_END()
     };
@@ -69,7 +69,7 @@ int main(int argc, char **argv)
         show_usage_and_die(program_usage_string, program_options);
 
     const char *tool_name;
-    if (strcmp(target, "strata") == 0)
+    if (strcmp(target, "rhts") == 0)
         tool_name = "reporter-rhtsupport";
     else
     if (strcmp(target, "bugzilla") == 0)
