abrt/libreport

commit 05e9c92733a0dd4cafbc25d3cada0caea54325a2

Replace libreport_xasprintf with g_strdup_printf
Signed-off-by: Michal Fabik <mfabik@redhat.com>

src/lib/curl.c
@@ -38,7 +38,7 @@ static char*
 check_curl_error(CURLcode err, const char* msg)
 {
     if (err)
-        return libreport_xasprintf("%s: %s", msg, curl_easy_strerror(err));
+        return g_strdup_printf("%s: %s", msg, curl_easy_strerror(err));
     return NULL;
 }
 
@@ -480,12 +480,11 @@ post(post_state_t *state,
     if (data_size != POST_DATA_FROMFILE_AS_FORM_DATA
         && data_size != POST_DATA_STRING_AS_FORM_DATA)
     {
-        char *content_type_header = libreport_xasprintf("Content-Type: %s", content_type);
+        g_autofree char *content_type_header = g_strdup_printf("Content-Type: %s", content_type);
         // Note: curl_slist_append() copies content_type_header
         httpheader_list = curl_slist_append(httpheader_list, content_type_header);
         if (!httpheader_list)
             error_msg_and_die("out of memory");
-        free(content_type_header);
     }
 
     for (; additional_headers && *additional_headers; additional_headers++)
@@ -687,16 +686,14 @@ char *libreport_upload_file_ext(post_state_t *state, const char *url, const char
                 (state->curl_result == CURLE_LOGIN_DENIED
                  || state->curl_result == CURLE_REMOTE_ACCESS_DENIED))
         {
-            char *msg = libreport_xasprintf(_("Please enter user name for '%s//%s':"), scheme, hostname);
+            g_autofree char *msg = g_strdup_printf(_("Please enter user name for '%s//%s':"), scheme, hostname);
             free(username);
             username = libreport_ask(msg);
-            free(msg);
             if (username != NULL && username[0] != '\0')
             {
-                msg = libreport_xasprintf(_("Please enter password for '%s//%s@%s':"), scheme, username, hostname);
+                msg = g_strdup_printf(_("Please enter password for '%s//%s@%s':"), scheme, username, hostname);
                 free(password);
                 password = libreport_ask_password(msg);
-                free(msg);
                 /* What about empty password? */
                 if (password != NULL && password[0] != '\0')
                 {

src/lib/steal_directory.c
@@ -47,11 +47,10 @@ struct dump_dir *libreport_steal_directory(const char *base_dir, const char *dum
 
     struct dump_dir *dd_dst;
     unsigned count = 100;
-    char *dst_dir_name = libreport_concat_path_file(base_dir, base_name);
+    g_autofree char *dst_dir_name = libreport_concat_path_file(base_dir, base_name);
     while (1)
     {
         dd_dst = dd_create(dst_dir_name, (uid_t)-1, DEFAULT_DUMP_DIR_MODE);
-        free(dst_dir_name);
         if (dd_dst)
             break;
         if (--count == 0)
@@ -61,7 +60,7 @@ struct dump_dir *libreport_steal_directory(const char *base_dir, const char *dum
         }
         struct timeval tv;
         gettimeofday(&tv, NULL);
-        dst_dir_name = libreport_xasprintf("%s/%s.%u", base_dir, base_name, (int)tv.tv_usec);
+        dst_dir_name = g_strdup_printf("%s/%s.%u", base_dir, base_name, (int)tv.tv_usec);
     }
 
     log_notice("Creating copy in '%s'", dd_dst->dd_dirname);

src/plugins/reporter-mantisbt.c
@@ -77,13 +77,11 @@ ask_mantisbt_credentials(mantisbt_settings_t *settings, const char *pre_message)
     free(settings->m_login);
     free(settings->m_password);
 
-    char *question = libreport_xasprintf("%s %s", pre_message, _("Please enter your MantisBT login:"));
+    g_autofree char *question = g_strdup_printf("%s %s", pre_message, _("Please enter your MantisBT login:"));
     settings->m_login = ask_mantisbt_login(question);
-    free(question);
 
-    question = libreport_xasprintf("%s %s '%s':", pre_message, _("Please enter the password for"), settings->m_login);
+    question = g_strdup_printf("%s %s '%s':", pre_message, _("Please enter the password for"), settings->m_login);
     settings->m_password = ask_mantisbt_password(question);
-    free(question);
 
     return;
 }
@@ -301,11 +299,11 @@ int main(int argc, char **argv)
     map_string_t *settings = libreport_new_map_string();
 
     {
-        char *local_conf = NULL;
+        g_autofree char *local_conf = NULL;
         if (!conf_file)
         {
             conf_file = g_list_append(conf_file, (char*) CONF_DIR"/plugins/mantisbt.conf");
-            local_conf = libreport_xasprintf("%s"USER_HOME_CONFIG_PATH"/mantisbt.conf", getenv("HOME"));
+            local_conf = g_strdup_printf("%s"USER_HOME_CONFIG_PATH"/mantisbt.conf", getenv("HOME"));
             conf_file = g_list_append(conf_file, local_conf);
         }
         while (conf_file)
@@ -316,7 +314,6 @@ int main(int argc, char **argv)
             log_debug("Loaded '%s'", fn);
             conf_file = g_list_delete_link(conf_file, conf_file);
         }
-        free(local_conf);
 
         struct dump_dir *dd = NULL;
         if (abrt_hash == NULL)
@@ -429,7 +426,7 @@ int main(int argc, char **argv)
         {
             g_autofree char *msg = NULL;
 
-            msg = libreport_xasprintf(_("This problem was already reported to MantisBT (see '%s')."
+            msg = g_strdup_printf(_("This problem was already reported to MantisBT (see '%s')."
                             " Do you still want to create a new issue?"),
                             url);
 
@@ -591,7 +588,7 @@ int main(int argc, char **argv)
                 return EXIT_FAILURE;
 
             log_warning(_("Adding attachments to issue %i"), new_id);
-            char *new_id_str = libreport_xasprintf("%u", new_id);
+            g_autofree char *new_id_str = g_strdup_printf("%u", new_id);
 
             for (GList *a = problem_report_get_attachments(pr); a != NULL; a = g_list_next(a))
             {
@@ -605,7 +602,6 @@ int main(int argc, char **argv)
                     mantisbt_attach_file(&mbt_settings, new_id_str, item_name, item->content);
             }
 
-            free(new_id_str);
             problem_report_free(pr);
             ii = mantisbt_issue_info_new();
             ii->mii_id = new_id;
@@ -667,28 +663,24 @@ int main(int argc, char **argv)
                 rating = libreport_xatou(rating_str);
             if (bt && rating > ii->mii_best_bt_rating)
             {
-                char *bug_id_str = libreport_xasprintf("%i", ii->mii_id);
+                g_autofree char *bug_id_str = g_strdup_printf("%i", ii->mii_id);
 
                 log_warning(_("Attaching better backtrace"));
 
                 // find unique filename of attachment
-                char *name = NULL;
+                g_autofree char *name = NULL;
                 for (int i = 0;; ++i)
                 {
                     if (i == 0)
-                        name = libreport_xasprintf("%s", FILENAME_BACKTRACE);
+                        name = g_strdup_printf("%s", FILENAME_BACKTRACE);
                     else
-                        name = libreport_xasprintf("%s%d", FILENAME_BACKTRACE, i);
+                        name = g_strdup_printf("%s%d", FILENAME_BACKTRACE, i);
 
                     if (g_list_find_custom(ii->mii_attachments, name, (GCompareFunc) strcmp) == NULL)
                         break;
 
-                    free(name);
                 }
                 mantisbt_attach_data(&mbt_settings, bug_id_str, name, bt, strlen(bt));
-
-                free(name);
-                free(bug_id_str);
             }
         }
         else
@@ -710,16 +702,14 @@ finish:
     if (dd)
     {
         report_result_t *result;
-        char *url;
 
         result = report_result_new_with_label_from_env("MantisBT");
-        url = libreport_xasprintf("%s/view.php?id=%u", mbt_settings.m_mantisbt_url, ii->mii_id);
+        g_autofree char *url = g_strdup_printf("%s/view.php?id=%u", mbt_settings.m_mantisbt_url, ii->mii_id);
 
         report_result_set_url(result, url);
 
         libreport_add_reported_to_entry(dd, result);
 
-        free(url);
         report_result_free(result);
         dd_close(dd);
     }

src/cli/cli-report.c
@@ -429,7 +429,7 @@ static void ask_for_missing_settings(const char *event_name)
             free(opt->eo_value);
             opt->eo_value = NULL;
 
-            char *question = libreport_xasprintf("%s %s:",
+            g_autofree char *question = g_strdup_printf("%s %s:",
                                              ec_get_screen_name(event_config) ? ec_get_screen_name(event_config) : event_name,
                                              (opt->eo_label) ? opt->eo_label : opt->eo_name);
             switch (opt->eo_type) {
@@ -453,8 +453,6 @@ static void ask_for_missing_settings(const char *event_name)
             case OPTION_TYPE_INVALID:
                 break;
             };
-
-            free(question);
         }
 
         g_list_free_full(err_list, (GDestroyNotify)free_invalid_options);
@@ -467,11 +465,10 @@ static void ask_for_missing_settings(const char *event_name)
         for (iter = err_list; iter; iter = iter -> next)
         {
             invalid_option_t *err_data = (invalid_option_t *)iter->data;
-            char *msg = libreport_xasprintf(_("Bad value for '%s': %s"),
+            g_autofree char *msg = g_strdup_printf(_("Bad value for '%s': %s"),
                                     err_data->invopt_name,
                                     err_data->invopt_error);
             libreport_alert(msg);
-            free(msg);
         }
 
         g_list_free_full(err_list, (GDestroyNotify)free_invalid_options);
@@ -668,11 +665,10 @@ static int run_event_on_dir_name_interactively(
 
         if (config->ec_sending_sensitive_data)
         {
-            char *msg = libreport_xasprintf(_("Event '%s' requires permission to send possibly sensitive data."
+            g_autofree char *msg = g_strdup_printf(_("Event '%s' requires permission to send possibly sensitive data."
                                     " Do you want to continue?"),
                         ec_get_screen_name(config) ? ec_get_screen_name(config) : event_name);
             bool ok = libreport_ask_yes_no(msg);
-            free(msg);
             if (!ok)
                 goto ret;
         }
@@ -715,9 +711,8 @@ static int choose_number_from_range(unsigned min, unsigned max, const char *mess
         if (min <= picked && picked <= max)
             return picked;
 
-        char *msg = libreport_xasprintf("%s (%u - %u)\n", _("You have chosen number out of range"), min, max);
+        g_autofree char *msg = g_strdup_printf("%s (%u - %u)\n", _("You have chosen number out of range"), min, max);
         libreport_alert(msg);
-        free(msg);
     }
 
     error_msg_and_die(_("Invalid input, exiting."));
@@ -884,7 +879,7 @@ int select_and_run_workflow(const char *dump_dir_name, GHashTable *workflows, in
     workflow_t *workflow;
     GList *events;
     const char *workflow_name;
-    char *environment_variable;
+    g_autofree char *environment_variable = NULL;
     struct run_event_state *run_state;
     int retval;
 

src/gtk-helpers/config_dialog.c
@@ -174,12 +174,12 @@ void libreport_add_item_to_config_liststore(gpointer cdialog, gpointer inf, gpoi
     config_item_info_t *info = (config_item_info_t *)inf;
 
     log_notice("adding '%s' to workflow list\n", ci_get_screen_name(info));
-    char *label;
+    g_autofree char *label = NULL;
     if (ci_get_screen_name(info) != NULL && ci_get_description(info) != NULL)
-        label = libreport_xasprintf("<b>%s</b>\n%s",ci_get_screen_name(info), ci_get_description(info));
+        label = g_strdup_printf("<b>%s</b>\n%s",ci_get_screen_name(info), ci_get_description(info));
     else
         //if event has no xml description
-        label = libreport_xasprintf("<b>%s</b>\n%s", _("No description available"), ci_get_name(info));
+        label = g_strdup_printf("<b>%s</b>\n%s", _("No description available"), ci_get_name(info));
 
     GtkTreeIter iter;
     gtk_list_store_append(list_store, &iter);
@@ -188,7 +188,6 @@ void libreport_add_item_to_config_liststore(gpointer cdialog, gpointer inf, gpoi
                       COLUMN_NAME, ci_get_name(info),
                       CONFIG_DIALOG, cdialog,
                       -1);
-    free(label);
 }
 
 //filters configuration - show only those with configurable options trac#881

src/gtk-helpers/event_config_dialog.c
@@ -330,7 +330,7 @@ config_dialog_t *create_event_config_dialog(const char *event_name, GtkWindow *p
 
     GtkWindow *parent_window = parent ? parent : g_event_list_window;
 
-    char *window_title = libreport_xasprintf("%s - Reporting Configuration",
+    g_autofree char *window_title = g_strdup_printf("%s - Reporting Configuration",
             ec_get_screen_name(event) ? ec_get_screen_name(event) : event_name);
 
     GtkWidget *dialog = gtk_dialog_new_with_buttons(
@@ -342,8 +342,6 @@ config_dialog_t *create_event_config_dialog(const char *event_name, GtkWindow *p
                         _("_OK"),
                         GTK_RESPONSE_APPLY,
                         NULL);
-
-    free(window_title);
 
     gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_APPLY);
 

src/gtk-helpers/problem_details_widget.c
@@ -195,9 +195,8 @@ problem_details_widget_add_binary(ProblemDetailsWidget *self, const char *label,
     }
 
     gchar *size = g_format_size_full((long long)statbuf.st_size, G_FORMAT_SIZE_IEC_UNITS);
-    char *msg = libreport_xasprintf(_("$DATA_DIRECTORY/%s (binary file, %s)"), label, size);
+    g_autofree char *msg = g_strdup_printf(_("$DATA_DIRECTORY/%s (binary file, %s)"), label, size);
     problem_details_widget_add_single_line(self, label, msg);
-    free(msg);
     g_free(size);
 }
 
@@ -303,16 +302,15 @@ problem_details_widget_populate(ProblemDetailsWidget *self)
         const char *username = problem_data_get_content_or_NULL(
                 self->priv->problem_data, "username");
 
-        char *line = NULL;
+        g_autofree char *line = NULL;
         if (uid && username)
-            line = libreport_xasprintf("%s (%s)", username, uid);
+            line = g_strdup_printf("%s (%s)", username, uid);
         else if (!uid && !username)
             line = libreport_xstrdup("unknown user");
         else
-            line = libreport_xasprintf("%s", uid ? uid : username);
+            line = g_strdup_printf("%s", uid ? uid : username);
 
         problem_details_widget_add_single_line(self, "user", line);
-        free(line);
     }
 
     { /* Type/Analyzer: CCpp */
@@ -322,13 +320,13 @@ problem_details_widget_populate(ProblemDetailsWidget *self)
                 self->priv->problem_data, FILENAME_ANALYZER);
 
         char *label = NULL;
-        char *line = NULL;
+        g_autofree char *line = NULL;
         if (type != NULL && analyzer != NULL)
         {
             if (strcmp(type, analyzer) != 0)
             {
                 label = libreport_xstrdup("type/analyzer");
-                line = libreport_xasprintf("%s/%s", type, analyzer);
+                line = g_strdup_printf("%s/%s", type, analyzer);
             }
             else
             {
@@ -344,7 +342,6 @@ problem_details_widget_populate(ProblemDetailsWidget *self)
 
         problem_details_widget_add_single_line(self, label, line);
 
-        free(line);
         free(label);
     }
 

src/gtk-helpers/workflow_config_dialog.c
@@ -80,7 +80,7 @@ config_dialog_t *create_workflow_config_dialog(const char *workflow_name, GtkWin
 
     GtkWindow *parent_window = parent ? parent : g_parent_window;
 
-    char *window_title = libreport_xasprintf("%s - Reporting Configuration",
+    g_autofree char *window_title = g_strdup_printf("%s - Reporting Configuration",
             wf_get_screen_name(workflow) ? wf_get_screen_name(workflow) : workflow_name);
 
     GtkWidget *dialog = gtk_dialog_new_with_buttons(
@@ -92,8 +92,6 @@ config_dialog_t *create_workflow_config_dialog(const char *workflow_name, GtkWin
                         _("_OK"),
                         GTK_RESPONSE_APPLY,
                         NULL);
-
-    free(window_title);
 
     gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_APPLY);
 

commit f7a2807ba5158442ac9aa747f861947efafbc50f

lib,reporter-mantisbt: Fix memory leaks
More fallout from 05e9c9273.

src/lib/curl.c
@@ -685,12 +685,14 @@ char *libreport_upload_file_ext(post_state_t *state, const char *url, const char
                 (state->curl_result == CURLE_LOGIN_DENIED
                  || state->curl_result == CURLE_REMOTE_ACCESS_DENIED))
         {
-            g_autofree char *msg = g_strdup_printf(_("Please enter user name for '%s//%s':"), scheme, hostname);
+            char *msg = g_strdup_printf(_("Please enter user name for '%s//%s':"), scheme, hostname);
             username = libreport_ask(msg);
+            free(msg);
             if (username != NULL && username[0] != '\0')
             {
                 msg = g_strdup_printf(_("Please enter password for '%s//%s@%s':"), scheme, username, hostname);
                 password = libreport_ask_password(msg);
+                free(msg);
                 /* What about empty password? */
                 if (password != NULL && password[0] != '\0')
                 {

src/lib/steal_directory.c
@@ -47,10 +47,11 @@ struct dump_dir *libreport_steal_directory(const char *base_dir, const char *dum
 
     struct dump_dir *dd_dst;
     unsigned count = 100;
-    g_autofree char *dst_dir_name = g_build_filename(base_dir ? base_dir : "", base_name, NULL);
+    char *dst_dir_name = g_build_filename(base_dir ? base_dir : "", base_name, NULL);
     while (1)
     {
         dd_dst = dd_create(dst_dir_name, (uid_t)-1, DEFAULT_DUMP_DIR_MODE);
+        free(dst_dir_name);
         if (dd_dst)
             break;
         if (--count == 0)

src/plugins/reporter-mantisbt.c
@@ -695,6 +695,7 @@ int main(int argc, char **argv)
                     if (g_list_find_custom(ii->mii_attachments, name, (GCompareFunc) strcmp) == NULL)
                         break;
 
+                    free(name);
                 }
                 mantisbt_attach_data(&mbt_settings, bug_id_str, name, bt, strlen(bt));
             }