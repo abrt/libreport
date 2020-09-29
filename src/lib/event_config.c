abrt/libreport

commit 05e9c92733a0dd4cafbc25d3cada0caea54325a2

Replace libreport_xasprintf with g_strdup_printf
Signed-off-by: Michal Fabik <mfabik@redhat.com>

src/lib/event_config.c
@@ -403,7 +403,7 @@ static char *validate_event_option(event_option_t *opt)
 
     const gchar *s = NULL;
     if (!g_utf8_validate(opt->eo_value, -1, &s))
-            return libreport_xasprintf(_("Invalid utf8 character '%c'"), *s);
+            return g_strdup_printf(_("Invalid utf8 character '%c'"), *s);
 
     switch (opt->eo_type) {
     case OPTION_TYPE_TEXT:
@@ -416,7 +416,7 @@ static char *validate_event_option(event_option_t *opt)
         long r = strtol(opt->eo_value, &endptr, 10);
         (void) r;
         if (errno != 0 || endptr == opt->eo_value || *endptr != '\0')
-            return libreport_xasprintf(_("Invalid number '%s'"), opt->eo_value);
+            return g_strdup_printf(_("Invalid number '%s'"), opt->eo_value);
 
         break;
     }
@@ -431,7 +431,7 @@ static char *validate_event_option(event_option_t *opt)
          && strcmp(opt->eo_value, "1") != 0
          && strcmp(opt->eo_value, "0") != 0
         ) {
-            return libreport_xasprintf(_("Invalid boolean value '%s'"), opt->eo_value);
+            return g_strdup_printf(_("Invalid boolean value '%s'"), opt->eo_value);
         }
         break;
     case OPTION_TYPE_HINT_HTML:
@@ -493,8 +493,8 @@ bool check_problem_rating_usability(const event_config_t *cfg,
 {
     INITIALIZE_LIBREPORT();
 
-    char *tmp_desc = NULL;
-    char *tmp_detail = NULL;
+    g_autofree char *tmp_desc = NULL;
+    g_autofree char *tmp_detail = NULL;
     bool result = true;
 
     if (!cfg)
@@ -511,7 +511,7 @@ bool check_problem_rating_usability(const event_config_t *cfg,
     const long rating = strtol(rating_str, &endptr, 10);
     if (errno != 0 || endptr == rating_str || *endptr != '\0')
     {
-        tmp_desc = libreport_xasprintf(
+        tmp_desc = g_strdup_printf(
                 _("The problem cannot be reported due to an invalid data. " \
                   "'%s' file does not contain a number."),
                 FILENAME_RATING);
@@ -533,7 +533,7 @@ bool check_problem_rating_usability(const event_config_t *cfg,
 
         const char *package = problem_data_get_content_or_NULL(pd, FILENAME_PACKAGE);
         if (package && package[0])
-            tmp_detail = libreport_xasprintf(_("Please try to install debuginfo manually using the command: \"debuginfo-install %s\" and try again."), package);
+            tmp_detail = g_strdup_printf(_("Please try to install debuginfo manually using the command: \"debuginfo-install %s\" and try again."), package);
         else
             tmp_detail = libreport_xstrdup(_("A proper debuginfo is probably missing or the coredump is corrupted."));
 
@@ -545,13 +545,9 @@ bool check_problem_rating_usability(const event_config_t *cfg,
 finish:
     if (description)
         *description = tmp_desc;
-    else
-        free(tmp_desc);
 
     if (detail)
         *detail = tmp_detail;
-    else
-        free(tmp_detail);
 
     return result;
 }

src/cli/cli-report.c
@@ -429,7 +429,7 @@ static void ask_for_missing_settings(const char *event_name)
             free(opt->eo_value);
             opt->eo_value = NULL;
 
-            char *question = libreport_xasprintf("%s %s:",
+            g_autofree char *question = g_strdup_printf("%s %s:",
                                              ec_get_screen_name(event_config) ? ec_get_screen_name(event_config) : event_name,
                                              (opt->eo_label) ? opt->eo_label : opt->eo_name);
             switch (opt->eo_type) {
@@ -453,8 +453,6 @@ static void ask_for_missing_settings(const char *event_name)
             case OPTION_TYPE_INVALID:
                 break;
             };
-
-            free(question);
         }
 
         g_list_free_full(err_list, (GDestroyNotify)free_invalid_options);
@@ -467,11 +465,10 @@ static void ask_for_missing_settings(const char *event_name)
         for (iter = err_list; iter; iter = iter -> next)
         {
             invalid_option_t *err_data = (invalid_option_t *)iter->data;
-            char *msg = libreport_xasprintf(_("Bad value for '%s': %s"),
+            g_autofree char *msg = g_strdup_printf(_("Bad value for '%s': %s"),
                                     err_data->invopt_name,
                                     err_data->invopt_error);
             libreport_alert(msg);
-            free(msg);
         }
 
         g_list_free_full(err_list, (GDestroyNotify)free_invalid_options);
@@ -668,11 +665,10 @@ static int run_event_on_dir_name_interactively(
 
         if (config->ec_sending_sensitive_data)
         {
-            char *msg = libreport_xasprintf(_("Event '%s' requires permission to send possibly sensitive data."
+            g_autofree char *msg = g_strdup_printf(_("Event '%s' requires permission to send possibly sensitive data."
                                     " Do you want to continue?"),
                         ec_get_screen_name(config) ? ec_get_screen_name(config) : event_name);
             bool ok = libreport_ask_yes_no(msg);
-            free(msg);
             if (!ok)
                 goto ret;
         }
@@ -715,9 +711,8 @@ static int choose_number_from_range(unsigned min, unsigned max, const char *mess
         if (min <= picked && picked <= max)
             return picked;
 
-        char *msg = libreport_xasprintf("%s (%u - %u)\n", _("You have chosen number out of range"), min, max);
+        g_autofree char *msg = g_strdup_printf("%s (%u - %u)\n", _("You have chosen number out of range"), min, max);
         libreport_alert(msg);
-        free(msg);
     }
 
     error_msg_and_die(_("Invalid input, exiting."));
@@ -884,7 +879,7 @@ int select_and_run_workflow(const char *dump_dir_name, GHashTable *workflows, in
     workflow_t *workflow;
     GList *events;
     const char *workflow_name;
-    char *environment_variable;
+    g_autofree char *environment_variable = NULL;
     struct run_event_state *run_state;
     int retval;
 

src/gtk-helpers/config_dialog.c
@@ -174,12 +174,12 @@ void libreport_add_item_to_config_liststore(gpointer cdialog, gpointer inf, gpoi
     config_item_info_t *info = (config_item_info_t *)inf;
 
     log_notice("adding '%s' to workflow list\n", ci_get_screen_name(info));
-    char *label;
+    g_autofree char *label = NULL;
     if (ci_get_screen_name(info) != NULL && ci_get_description(info) != NULL)
-        label = libreport_xasprintf("<b>%s</b>\n%s",ci_get_screen_name(info), ci_get_description(info));
+        label = g_strdup_printf("<b>%s</b>\n%s",ci_get_screen_name(info), ci_get_description(info));
     else
         //if event has no xml description
-        label = libreport_xasprintf("<b>%s</b>\n%s", _("No description available"), ci_get_name(info));
+        label = g_strdup_printf("<b>%s</b>\n%s", _("No description available"), ci_get_name(info));
 
     GtkTreeIter iter;
     gtk_list_store_append(list_store, &iter);
@@ -188,7 +188,6 @@ void libreport_add_item_to_config_liststore(gpointer cdialog, gpointer inf, gpoi
                       COLUMN_NAME, ci_get_name(info),
                       CONFIG_DIALOG, cdialog,
                       -1);
-    free(label);
 }
 
 //filters configuration - show only those with configurable options trac#881

src/gtk-helpers/event_config_dialog.c
@@ -330,7 +330,7 @@ config_dialog_t *create_event_config_dialog(const char *event_name, GtkWindow *p
 
     GtkWindow *parent_window = parent ? parent : g_event_list_window;
 
-    char *window_title = libreport_xasprintf("%s - Reporting Configuration",
+    g_autofree char *window_title = g_strdup_printf("%s - Reporting Configuration",
             ec_get_screen_name(event) ? ec_get_screen_name(event) : event_name);
 
     GtkWidget *dialog = gtk_dialog_new_with_buttons(
@@ -342,8 +342,6 @@ config_dialog_t *create_event_config_dialog(const char *event_name, GtkWindow *p
                         _("_OK"),
                         GTK_RESPONSE_APPLY,
                         NULL);
-
-    free(window_title);
 
     gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_APPLY);
 

src/gtk-helpers/problem_details_widget.c
@@ -195,9 +195,8 @@ problem_details_widget_add_binary(ProblemDetailsWidget *self, const char *label,
     }
 
     gchar *size = g_format_size_full((long long)statbuf.st_size, G_FORMAT_SIZE_IEC_UNITS);
-    char *msg = libreport_xasprintf(_("$DATA_DIRECTORY/%s (binary file, %s)"), label, size);
+    g_autofree char *msg = g_strdup_printf(_("$DATA_DIRECTORY/%s (binary file, %s)"), label, size);
     problem_details_widget_add_single_line(self, label, msg);
-    free(msg);
     g_free(size);
 }
 
@@ -303,16 +302,15 @@ problem_details_widget_populate(ProblemDetailsWidget *self)
         const char *username = problem_data_get_content_or_NULL(
                 self->priv->problem_data, "username");
 
-        char *line = NULL;
+        g_autofree char *line = NULL;
         if (uid && username)
-            line = libreport_xasprintf("%s (%s)", username, uid);
+            line = g_strdup_printf("%s (%s)", username, uid);
         else if (!uid && !username)
             line = libreport_xstrdup("unknown user");
         else
-            line = libreport_xasprintf("%s", uid ? uid : username);
+            line = g_strdup_printf("%s", uid ? uid : username);
 
         problem_details_widget_add_single_line(self, "user", line);
-        free(line);
     }
 
     { /* Type/Analyzer: CCpp */
@@ -322,13 +320,13 @@ problem_details_widget_populate(ProblemDetailsWidget *self)
                 self->priv->problem_data, FILENAME_ANALYZER);
 
         char *label = NULL;
-        char *line = NULL;
+        g_autofree char *line = NULL;
         if (type != NULL && analyzer != NULL)
         {
             if (strcmp(type, analyzer) != 0)
             {
                 label = libreport_xstrdup("type/analyzer");
-                line = libreport_xasprintf("%s/%s", type, analyzer);
+                line = g_strdup_printf("%s/%s", type, analyzer);
             }
             else
             {
@@ -344,7 +342,6 @@ problem_details_widget_populate(ProblemDetailsWidget *self)
 
         problem_details_widget_add_single_line(self, label, line);
 
-        free(line);
         free(label);
     }
 

src/gtk-helpers/workflow_config_dialog.c
@@ -80,7 +80,7 @@ config_dialog_t *create_workflow_config_dialog(const char *workflow_name, GtkWin
 
     GtkWindow *parent_window = parent ? parent : g_parent_window;
 
-    char *window_title = libreport_xasprintf("%s - Reporting Configuration",
+    g_autofree char *window_title = g_strdup_printf("%s - Reporting Configuration",
             wf_get_screen_name(workflow) ? wf_get_screen_name(workflow) : workflow_name);
 
     GtkWidget *dialog = gtk_dialog_new_with_buttons(
@@ -92,8 +92,6 @@ config_dialog_t *create_workflow_config_dialog(const char *workflow_name, GtkWin
                         _("_OK"),
                         GTK_RESPONSE_APPLY,
                         NULL);
-
-    free(window_title);
 
     gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_APPLY);
 

src/gui-wizard-gtk/wizard.c
@@ -329,14 +329,14 @@ static void update_window_title(void)
     /* prgname can be null according to gtk documentation */
     const char *prgname = g_get_prgname();
     const char *reason = problem_data_get_content_or_NULL(g_cd, FILENAME_REASON);
-    g_autofree char *title = libreport_xasprintf("%s - %s", (reason ? reason : g_dump_dir_name),
+    g_autofree char *title = g_strdup_printf("%s - %s", (reason ? reason : g_dump_dir_name),
             (prgname ? prgname : "report"));
     gtk_window_set_title(g_wnd_assistant, title);
 }
 
 static bool ask_continue_before_steal(const char *base_dir, const char *dump_dir)
 {
-    g_autofree char *msg = libreport_xasprintf(
+    g_autofree char *msg = g_strdup_printf(
             _("Need writable directory, but '%s' is not writable."
               " Move it to '%s' and operate on the moved data?"),
             dump_dir, base_dir);
@@ -1017,7 +1017,7 @@ static void append_item_to_ls_details(gpointer name, gpointer value, gpointer da
         if (stat(item->content, &statbuf) == 0)
         {
             stats->filesize += statbuf.st_size;
-            g_autofree char *msg = libreport_xasprintf(
+            g_autofree char *msg = g_strdup_printf(
                     _("(binary file, %llu bytes)"), (long long)statbuf.st_size);
             gtk_list_store_set(g_ls_details, &iter,
                                   DETAIL_COLUMN_NAME, (char *)name,
@@ -1115,7 +1115,7 @@ void update_gui_state_from_problem_data(int flags)
     const char *not_reportable = problem_data_get_content_or_NULL(g_cd,
                                                                   FILENAME_NOT_REPORTABLE);
 
-    g_autofree char *t = libreport_xasprintf("%s%s%s",
+    g_autofree char *t = g_strdup_printf("%s%s%s",
                         not_reportable ? : "",
                         not_reportable ? " " : "",
                         reason ? : _("(no description)"));
@@ -1125,7 +1125,7 @@ void update_gui_state_from_problem_data(int flags)
     gtk_list_store_clear(g_ls_details);
     struct cd_stats stats = { 0 };
     g_hash_table_foreach(g_cd, append_item_to_ls_details, &stats);
-    g_autofree char *msg = libreport_xasprintf(
+    g_autofree char *msg = g_strdup_printf(
             _("%llu bytes, %u files"), (long long)stats.filesize, stats.filecount);
     gtk_label_set_text(g_lbl_size, msg);
 
@@ -1392,7 +1392,7 @@ static void update_command_run_log(const char* message, struct analyze_event_dat
         gtk_label_set_text(g_lbl_event_log, message);
 
     /* Don't append new line behind single dot */
-    const char *log_msg = it_is_a_dot ? message : libreport_xasprintf("%s\n", message);
+    const char *log_msg = it_is_a_dot ? message : g_strdup_printf("%s\n", message);
     append_to_textview(g_tv_event_log, log_msg);
     save_to_event_log(evd, log_msg);
 
@@ -1415,7 +1415,7 @@ static char *run_event_gtk_logging(char *log_line, void *param)
 
 static void log_request_response_communication(const char *request, const char *response, struct analyze_event_data *evd)
 {
-    g_autofree char *message = libreport_xasprintf(response ? "%s '%s'" : "%s", request,
+    g_autofree char *message = g_strdup_printf(response ? "%s '%s'" : "%s", request,
             response);
     update_command_run_log(message, evd);
 }
@@ -1766,7 +1766,7 @@ static void start_event_run(const char *event_name)
         free_run_event_state(state);
 
         log_warning("No processing commands specified. Processing halted.");
-        g_autofree char *msg = libreport_xasprintf(
+        g_autofree char *msg = g_strdup_printf(
                 _("No commands could be found for processsing the crashdump.\n"));
         append_to_textview(g_tv_event_log, msg);
 
@@ -1804,7 +1804,7 @@ static void start_event_run(const char *event_name)
 
         log_notice("No matching actions for event '%s'; skipping.", event_name);
 
-        g_autofree char *msg = libreport_xasprintf(
+        g_autofree char *msg = g_strdup_printf(
                 _("--- Skipping %s ---\n"
                   "No matching actions found for this event.\n\n"),
                 event_name);
@@ -1842,7 +1842,7 @@ static void start_event_run(const char *event_name)
 
     gtk_label_set_text(g_lbl_event_log, _("Processing..."));
     log_notice("running event '%s' on '%s'", event_name, g_dump_dir_name);
-    g_autofree char *msg = libreport_xasprintf("--- Running %s ---\n", event_name);
+    g_autofree char *msg = g_strdup_printf("--- Running %s ---\n", event_name);
     append_to_textview(g_tv_event_log, msg);
 
     /* don't bother testing if they are visible, this is faster */
@@ -1872,7 +1872,7 @@ static void add_widget_to_warning_area(GtkWidget    *widget,
 static void add_warning(const char   *warning,
                         GtkContainer *container)
 {
-    g_autofree char *label_str = libreport_xasprintf("• %s", warning);
+    g_autofree char *label_str = g_strdup_printf("• %s", warning);
     GtkWidget *warning_lbl = gtk_label_new(NULL);
     gtk_label_set_markup(GTK_LABEL(warning_lbl), label_str);
 
@@ -2141,7 +2141,7 @@ static void search_item_to_list_store_item(GtkListStore *store, GtkTreeIter *new
     g_autofree gchar *suffix = g_markup_escape_text(tmp, /*NULL terminated string*/-1);
     g_free(tmp);
 
-    char *content = libreport_xasprintf("%s<span foreground=\"red\">%s</span>%s", prefix, text, suffix);
+    char *content = g_strdup_printf("%s<span foreground=\"red\">%s</span>%s", prefix, text, suffix);
 
     gtk_text_iter_free(end);
     gtk_text_iter_free(beg);
@@ -2562,7 +2562,7 @@ static void add_workflow_buttons(GtkBox *box, GHashTable *workflows, GCallback f
     for (GList *wf_iter = wf_list; wf_iter; wf_iter = g_list_next(wf_iter))
     {
         workflow_t *w = (workflow_t *)wf_iter->data;
-        g_autofree char *btn_label = libreport_xasprintf("<b>%s</b>\n%s", wf_get_screen_name(w),
+        g_autofree char *btn_label = g_strdup_printf("<b>%s</b>\n%s", wf_get_screen_name(w),
                 wf_get_description(w));
         GtkWidget *button = gtk_button_new_with_label(btn_label);
         GList *children = gtk_container_get_children(GTK_CONTAINER(button));
@@ -2615,7 +2615,7 @@ static bool get_sensitive_data_permission(const char *event_name)
     if (screen_name)
         event_name = screen_name;
 
-    g_autofree char *msg = libreport_xasprintf(
+    g_autofree char *msg = g_strdup_printf(
             _("Event '%s' requires permission to send possibly sensitive data.\n"
               "Do you want to continue?"),
             event_name);
@@ -2662,7 +2662,7 @@ static gint select_next_page_no(gint current_page_no)
         if (problem_data_get_content_or_NULL(g_cd, FILENAME_NOT_REPORTABLE))
         {
 
-            char *msg = libreport_xasprintf(_("This problem should not be reported "
+            char *msg = g_strdup_printf(_("This problem should not be reported "
                             "(it is likely a known problem). %s"),
                             problem_data_get_content_or_NULL(g_cd, FILENAME_NOT_REPORTABLE)
             );
@@ -2885,7 +2885,7 @@ static void on_btn_add_file(GtkButton *button)
         struct stat statbuf;
         if (stat(filename, &statbuf) != 0 || !S_ISREG(statbuf.st_mode))
         {
-            message = libreport_xasprintf(_("'%s' is not an ordinary file"), filename);
+            message = g_strdup_printf(_("'%s' is not an ordinary file"), filename);
             goto show_msgbox;
         }
 
@@ -2904,7 +2904,7 @@ static void on_btn_add_file(GtkButton *button)
                     off_t r = libreport_copy_file(filename, new_name, 0666);
                     if (r < 0)
                     {
-                        message = libreport_xasprintf(_("Can't copy '%s': %s"), filename, strerror(errno));
+                        message = g_strdup_printf(_("Can't copy '%s': %s"), filename, strerror(errno));
                         unlink(new_name);
                     }
                     if (!message)
@@ -2918,7 +2918,7 @@ static void on_btn_add_file(GtkButton *button)
             }
         }
         else
-            message = libreport_xasprintf(_("Item '%s' already exists and is not modifiable"), basename);
+            message = g_strdup_printf(_("Item '%s' already exists and is not modifiable"), basename);
 
         if (message)
         {

src/include/internal_libreport.h
@@ -323,7 +323,6 @@ off_t libreport_xlseek(int fd, off_t offset, int whence);
 void libreport_xchdir(const char *path);
 
 char *libreport_xvasprintf(const char *format, va_list p);
-char *libreport_xasprintf(const char *format, ...);
 
 void libreport_xsetenv(const char *key, const char *value);
 /*

commit 9e982a8c88da8bfc83adc84d104939f164badd02

event_config: Null autofree pointers before returning
The pointers to strings in the function check_problem_rating_usability()
need to be nullified before the function returns as they are declared
for auto-cleanup.

This change fixes a double-free condition in which the returned strings
were attempted to be freed again in the caller,
is_backtrace_rating_usable().

Bug was introduced in 05e9c9273.

Resolves rhbz#1883410

src/lib/event_config.c
@@ -541,10 +541,10 @@ bool check_problem_rating_usability(const event_config_t *cfg,
 
 finish:
     if (description)
-        *description = tmp_desc;
+        *description = g_steal_pointer(&tmp_desc);
 
     if (detail)
-        *detail = tmp_detail;
+        *detail = g_steal_pointer(&tmp_detail);
 
     return result;
 }