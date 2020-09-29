abrt/libreport

commit df386b09745c162fbe36561105225f1f8c7b0370

Replace libreport_xstrndup with g_strndup
Signed-off-by: Michal Fabik <mfabik@redhat.com>

src/gui-wizard-gtk/wizard.c
@@ -462,7 +462,7 @@ static void append_to_textview(GtkTextView *tv, const char *str)
         GtkTextTag *tag;
         tag = gtk_text_buffer_create_tag(tb, NULL, "foreground", "blue",
                                          "underline", PANGO_UNDERLINE_SINGLE, NULL);
-        char *url = libreport_xstrndup(t->start, t->len);
+        g_autofree char *url = g_strndup(t->start, t->len);
         g_object_set_data(G_OBJECT(tag), "url", url);
 
         gtk_text_buffer_insert_with_tags(tb, &text_iter, url, -1, tag, NULL);

src/include/internal_libreport.h
@@ -306,7 +306,6 @@ int libreport_close_on_exec_on(int fd);
 void *libreport_xmalloc(size_t size);
 void *libreport_xrealloc(void *ptr, size_t size);
 void *libreport_xzalloc(size_t size);
-char *libreport_xstrndup(const char *s, int n);
 char *libreport_xstrdup_between(const char *s, const char *open, const char *close);
 
 void libreport_xpipe(int filedes[2]);

src/lib/curl.c
@@ -161,7 +161,7 @@ save_headers(void *buffer_pv, size_t count, size_t nmemb, void *ptr)
     post_state_t* state = (post_state_t*)ptr;
     size_t size = count * nmemb;
 
-    char *h = libreport_xstrndup((char*)buffer_pv, size);
+    g_autofree char *h = g_strndup((char*)buffer_pv, size);
     strchrnul(h, '\r')[0] = '\0';
     strchrnul(h, '\n')[0] = '\0';
 

src/lib/dump_dir.c
@@ -938,7 +938,7 @@ static char* rm_trailing_slashes(const char *dir)
     unsigned len = strlen(dir);
     while (len != 0 && dir[len-1] == '/')
         len--;
-    return libreport_xstrndup(dir, len);
+    return g_strndup(dir, len);
 }
 
 static struct dump_dir *dd_do_open(struct dump_dir *dd, const char *dir, int flags)

src/lib/event_xml_parser.c
@@ -105,7 +105,7 @@ static char *get_element_lang(struct my_parse_data *parse_data, const gchar **at
              && strncmp(att_values[i], parse_data->cur_locale, short_locale_end - parse_data->cur_locale) == 0
             ) {
                 log_debug("found translation for shortlocale: %s", parse_data->cur_locale);
-                return libreport_xstrndup(att_values[i], short_locale_end - parse_data->cur_locale);
+                return g_strndup(att_values[i], short_locale_end - parse_data->cur_locale);
             }
         }
     }
@@ -289,7 +289,7 @@ static void text(GMarkupParseContext *context,
     event_config_t *ui = parse_data->event_config.values;
 
     const gchar *inner_element = g_markup_parse_context_get_element(context);
-    char *text_copy = libreport_xstrndup(text, text_len);
+    g_autofree char *text_copy = g_strndup(text, text_len);
     event_option_t *opt = parse_data->cur_option.values;
     if (opt)
     {
@@ -386,7 +386,6 @@ static void text(GMarkupParseContext *context,
 
                         log_info("event name:'%s'", text_copy);
                         ec_set_screen_name(ui, text_copy);
-                        free(text_copy);
                         text_copy = NULL;
                     }
                 }
@@ -410,7 +409,6 @@ static void text(GMarkupParseContext *context,
                             (strcmp(parse_data->attribute_lang, parse_data->cur_locale) == 0);
 
                         ec_set_description(ui, text_copy);
-                        free(text_copy);
                         text_copy = NULL;
                     }
                 }
@@ -435,7 +433,6 @@ static void text(GMarkupParseContext *context,
                             (strcmp(parse_data->attribute_lang, parse_data->cur_locale) == 0);
 
                         ec_set_long_desc(ui, text_copy);
-                        free(text_copy);
                         text_copy = NULL;
                     }
                 }
@@ -497,7 +494,6 @@ static void text(GMarkupParseContext *context,
             ui->ec_requires_details = libreport_string_to_bool(text_copy);
         }
     }
-    free(text_copy);
 }
 
   // Called for strings that should be re-saved verbatim in this same

src/lib/is_in_comma_separated_list.c
@@ -43,9 +43,8 @@ bool libreport_is_in_comma_separated_list_of_glob_patterns(const char *value, co
     while (*list)
     {
         const char *comma = strchrnul(list, ',');
-        char *pattern = libreport_xstrndup(list, comma - list);
+        g_autofree char *pattern = g_strndup(list, comma - list);
         int match = !fnmatch(pattern, value, /*flags:*/ 0);
-        free(pattern);
         if (match)
             return true;
         if (!*comma)

src/lib/parse_release.c
@@ -221,13 +221,13 @@ void libreport_parse_osinfo(const char *osinfo_bytes, map_string_t *osinfo)
             goto skip_line;
         }
 
-        char *key = libreport_xstrndup(cursor, key_end - cursor);
+        char *key = g_strndup(cursor, key_end - cursor);
         if (libreport_get_map_string_item_or_NULL(osinfo, key) != NULL)
         {
             log_notice("os-release:%u: redefines key '%s'", line, key);
         }
 
-        char *value = libreport_xstrndup(key_end + 1, value_end - key_end - 1);
+        char *value = g_strndup(key_end + 1, value_end - key_end - 1);
         unescape_osnifo_value(value, value);
 
         log_debug("os-release:%u: parsed line: '%s'='%s'", line, key, value);

src/lib/problem_report.c
@@ -145,7 +145,7 @@ split_string_on_char(const char *str, char ch)
     for (;;)
     {
         const char *delim = strchrnul(str, ch);
-        list = g_list_prepend(list, libreport_xstrndup(str, delim - str));
+        list = g_list_prepend(list, g_strndup(str, delim - str));
         if (*delim == '\0')
             break;
         str = delim + 1;

commit 02fb17a28d47f064bcd2cf5515df3d3f093751dc

gui-wizard-gtk: Don't autofree URL string
g_object_set_data() does not (and cannot) copy the data passed to it, so
once url is freed, a subsequent access to the 'url' tag leads to an
invalid read and segfault.

Bug was introduced in df386b097.

Resolves rhbz#1882328

src/gui-wizard-gtk/wizard.c
@@ -462,8 +462,8 @@ static void append_to_textview(GtkTextView *tv, const char *str)
         GtkTextTag *tag;
         tag = gtk_text_buffer_create_tag(tb, NULL, "foreground", "blue",
                                          "underline", PANGO_UNDERLINE_SINGLE, NULL);
-        g_autofree char *url = g_strndup(t->start, t->len);
-        g_object_set_data(G_OBJECT(tag), "url", url);
+        char *url = g_strndup(t->start, t->len);
+        g_object_set_data_full(G_OBJECT(tag), "url", url, g_free);
 
         gtk_text_buffer_insert_with_tags(tb, &text_iter, url, -1, tag, NULL);
 