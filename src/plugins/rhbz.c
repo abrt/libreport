abrt/libreport

commit 7aba6e53bbfeedaacd95bbaa5e0c5e325a3e6a8d

Use g_autofree where possible
Signed-off-by: Michal Fabik <mfabik@redhat.com>

src/plugins/rhbz.c
@@ -191,12 +191,11 @@ bool rhbz_login(struct abrt_xmlrpc *ax, const char *login, const char *password)
         return false;
     }
 
-    char *token = rhbz_bug_read_item("token", result, RHBZ_READ_STR);
+    g_autofree char *token = rhbz_bug_read_item("token", result, RHBZ_READ_STR);
     if (token != NULL)
     {
         log_debug("Adding session param Bugzilla_token");
         abrt_xmlrpc_client_add_session_param_string(&env, ax, "Bugzilla_token", token);
-        free(token);
     }
 
 //TODO: with URL like http://bugzilla.redhat.com (that is, with http: instead of https:)
@@ -269,7 +268,7 @@ unsigned rhbz_version(struct abrt_xmlrpc *ax)
 
     xmlrpc_value *result;
     result = abrt_xmlrpc_call(ax, "Bugzilla.version", "{}");
-    char *version = NULL;
+    g_autofree char *version = NULL;
     if (result)
         version = rhbz_bug_read_item("version", result, RHBZ_READ_STR);
     if (!result || !version)
@@ -290,8 +289,6 @@ unsigned rhbz_version(struct abrt_xmlrpc *ax)
 
         v[i] = strtoul(tok, NULL, 10);
     }
-
-    free(version);
 
     return BUGZILLA_VERSION(v[0], v[1], v[2]);
 }
@@ -409,7 +406,7 @@ GList *rhbz_bug_cc(xmlrpc_value* result_xml)
         if (!item)
             continue;
 
-        const char* cc = NULL;
+        g_autofree const char* cc = NULL;
         xmlrpc_read_string(&env, item, &cc);
         xmlrpc_DECREF(item);
         if (env.fault_occurred)
@@ -421,7 +418,6 @@ GList *rhbz_bug_cc(xmlrpc_value* result_xml)
             log_debug("member on cc is %s", cc);
             continue;
         }
-        free((char*)cc);
     }
     xmlrpc_DECREF(cc_member);
     return cc_list;
@@ -449,10 +445,9 @@ struct bug_info *rhbz_bug_info(struct abrt_xmlrpc *ax, int bug_id)
     xmlrpc_value *bugs_memb = rhbz_get_member("bugs", xml_bug_response);
     xmlrpc_value *bug_item = rhbz_array_item_at(bugs_memb, 0);
 
-    int *ret = (int*)rhbz_bug_read_item("id", bug_item,
+    g_autofree int *ret = (int*)rhbz_bug_read_item("id", bug_item,
                                         RHBZ_MANDATORY_MEMB | RHBZ_READ_INT);
     bz->bi_id = *ret;
-    free(ret);
     bz->bi_product = rhbz_bug_read_item("product", bug_item,
                                         RHBZ_MANDATORY_MEMB | RHBZ_READ_STR);
     bz->bi_reporter = rhbz_bug_read_item("creator", bug_item,
@@ -478,7 +473,6 @@ struct bug_info *rhbz_bug_info(struct abrt_xmlrpc *ax, int bug_id)
     }
 
     bz->bi_dup_id = (ret) ? *ret: -1;
-    free(ret);
 
     bz->bi_cc_list = rhbz_bug_cc(bug_item);
 
@@ -516,7 +510,7 @@ int rhbz_new_bug(struct abrt_xmlrpc *ax,
     if (!duphash) duphash    = problem_data_get_content_or_NULL(problem_data,
                                                                 "global_uuid");
 
-    char *summary = libreport_shorten_string_to_length(bzsummary, MAX_SUMMARY_LENGTH);
+    g_autofree char *summary = libreport_shorten_string_to_length(bzsummary, MAX_SUMMARY_LENGTH);
 
     GString *status_whiteboard = g_string_new(NULL);
     g_string_append_printf(status_whiteboard, "abrt_hash:%s;", duphash);
@@ -590,15 +584,13 @@ int rhbz_new_bug(struct abrt_xmlrpc *ax,
     xmlrpc_env_clean(&env);
 
     g_string_free(status_whiteboard, TRUE);
-    free(summary);
 
     if (!result)
         return -1;
 
-    int *r = rhbz_bug_read_item("id", result, RHBZ_MANDATORY_MEMB | RHBZ_READ_INT);
+    g_autofree int *r = rhbz_bug_read_item("id", result, RHBZ_MANDATORY_MEMB | RHBZ_READ_INT);
     xmlrpc_DECREF(result);
     int new_bug_id = *r;
-    free(r);
 
     log_warning(_("New bug id: %i"), new_bug_id);
     return new_bug_id;
@@ -680,17 +672,15 @@ int rhbz_attach_fd(struct abrt_xmlrpc *ax, const char *bug_id,
 
 //TODO: need to have a method of attaching huge files (IOW: 1Gb read isn't good).
 
-    char *data = g_malloc(size);
+    g_autofree char *data = g_malloc(size);
     ssize_t r = libreport_full_read(fd, data, size);
     if (r < 0)
     {
-        free(data);
         perror_msg("Can't read '%s'", att_name);
         return -1;
     }
 
     int res = rhbz_attach_blob(ax, bug_id, att_name, data, size, flags);
-    free(data);
     return res;
 }
 
@@ -859,11 +849,10 @@ xmlrpc_value *rhbz_search_duphash(struct abrt_xmlrpc *ax,
     if (component)
         g_string_append_printf(query, " component:\"%s\"", component);
 
-    char *s = g_string_free(query, FALSE);
+    g_autofree char *s = g_string_free(query, FALSE);
     log_debug("search for '%s'", s);
     xmlrpc_value *search = abrt_xmlrpc_call(ax, "Bug.search", "{s:s,s:(s)}", "quicksearch", s, "include_fields", "id");
 
-    free(s);
     xmlrpc_value *bugs = rhbz_get_member("bugs", search);
     xmlrpc_DECREF(search);
 

src/cli/cli-report.c
@@ -131,9 +131,8 @@ static void write_crash_report_field(FILE *fp, problem_data_t *problem_data,
     if (!(value->flags & CD_FLAG_ISEDITABLE))
         fprintf(fp, _("# This field is read only\n"));
 
-    char *escaped_content = escape(value->content);
+    g_autofree char *escaped_content = escape(value->content);
     fprintf(fp, "%s\n", escaped_content);
-    free(escaped_content);
 }
 
 /*
@@ -378,11 +377,10 @@ static int run_report_editor(problem_data_t *problem_data)
     off_t size = libreport_fstat_st_size_or_die(fileno(fp));
     if (size > INT_MAX/4)
 	    size = INT_MAX/4; /* paranoia */
-    char *text = g_malloc(size + 1);
+    g_autofree char *text = g_malloc(size + 1);
     if (fread(text, 1, size, fp) != size)
     {
         error_msg("Can't read '%s'", filename);
-        free(text);
         fclose(fp);
         return 2;
     }
@@ -398,7 +396,6 @@ static int run_report_editor(problem_data_t *problem_data)
     remove_comments_and_unescape(text);
     // Updates the crash report from the file text.
     int report_changed = read_crash_report(problem_data, text);
-    free(text);
     if (report_changed)
         puts(_("\nThe report has been updated"));
     else
@@ -527,8 +524,8 @@ static int is_not_reportable(problem_data_t *problem_data)
 
 static int is_backtrace_rating_usable(event_config_t *config, problem_data_t *problem_data)
 {
-    char *usability_description = NULL;
-    char *usability_detail = NULL;
+    g_autofree char *usability_description = NULL;
+    g_autofree char *usability_detail = NULL;
     const bool usable_rating = check_problem_rating_usability(config, problem_data,
                                                               &usability_description,
                                                               &usability_detail);
@@ -538,9 +535,6 @@ static int is_backtrace_rating_usable(event_config_t *config, problem_data_t *pr
         printf("%s\n", usability_description);
         printf("%s\n", usability_detail);
     }
-
-    free(usability_description);
-    free(usability_detail);
 
     return usable_rating;
 }
@@ -748,9 +742,8 @@ static char *select_event_name(GList *list_options)
 
 int select_and_run_one_event(const char *dump_dir_name, const char *pfx, int interactive)
 {
-    GList *list_events = list_possible_events_glist(dump_dir_name, pfx);
-    char *event_name = select_event_name(list_events);
-    libreport_list_free_with_free(list_events);
+    g_autolist (GList) list_events = list_possible_events_glist(dump_dir_name, pfx);
+    g_autofree char *event_name = select_event_name(list_events);
 
     struct run_event_state *run_state = new_run_event_state();
     run_state->logging_callback = do_log;
@@ -760,8 +753,6 @@ int select_and_run_one_event(const char *dump_dir_name, const char *pfx, int int
                 ;
 
     free_run_event_state(run_state);
-
-    free(event_name);
 
     return r;
 }
@@ -806,9 +797,8 @@ static int run_event_chain_real(struct run_event_state *run_state,
         else if (retval != 0 || !l_state.output_was_produced)
         {
             /* Program failed, or finished successfully with no output. */
-            char *msg = libreport_exit_status_as_string(event_name, run_state->process_status);
+            g_autofree char *msg = libreport_exit_status_as_string(event_name, run_state->process_status);
             fputs(msg, stdout);
-            free(msg);
         }
         if (retval != 0)
             break;

src/cli/cli.c
@@ -152,11 +152,10 @@ int main(int argc, char** argv)
             /* Note that dump_dir_name may be NULL here, it means
              * "show all possible events regardless of dir"
              */
-            char *events = list_possible_events(NULL, dump_dir_name, pfx);
+            g_autofree char *events = list_possible_events(NULL, dump_dir_name, pfx);
             if (!events)
                 return 1; /* error msg is already logged */
             fputs(events, stdout);
-            free(events);
             break;
         }
         case OPT_run_event: /* -e EVENT: run event */

src/client-python/reportclient/client.c
@@ -16,6 +16,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+#include <glib.h>
 #include <Python.h>
 
 #include "common.h"
@@ -41,14 +42,13 @@ PyObject *p_ask(PyObject *pself, PyObject *args)
         return NULL;
     }
 
-    char *response = libreport_ask(question);
+    g_autofree char *response = libreport_ask(question);
     if (!response)
     {
         Py_RETURN_NONE;
     }
 
     PyObject *r = Py_BuildValue("s", response);
-    free(response);
     return r;
 }
 
@@ -61,14 +61,13 @@ PyObject *p_ask_password(PyObject *pself, PyObject *args)
         return NULL;
     }
 
-    char *response = libreport_ask_password(question);
+    g_autofree char *response = libreport_ask_password(question);
     if (!response)
     {
         Py_RETURN_NONE;
     }
 
     PyObject *r = Py_BuildValue("s", response);
-    free(response);
     return r;
 }
 

src/gtk-helpers/problem_details_widget.c
@@ -194,10 +194,9 @@ problem_details_widget_add_binary(ProblemDetailsWidget *self, const char *label,
         return;
     }
 
-    gchar *size = g_format_size_full((long long)statbuf.st_size, G_FORMAT_SIZE_IEC_UNITS);
+    g_autofree gchar *size = g_format_size_full((long long)statbuf.st_size, G_FORMAT_SIZE_IEC_UNITS);
     g_autofree char *msg = g_strdup_printf(_("$DATA_DIRECTORY/%s (binary file, %s)"), label, size);
     problem_details_widget_add_single_line(self, label, msg);
-    g_free(size);
 }
 
 static void

src/gtk-helpers/secrets.c
@@ -208,9 +208,8 @@ static GVariant *dbus_call_sync(GDBusProxy *proxy, const gchar *method, GVariant
 static bool is_dbus_remote_error(GError *error, const char *type)
 {
     /* returns a malloced string and return value can be NULL */
-    char *remote_type = g_dbus_error_get_remote_error(error);
+    g_autofree char *remote_type = g_dbus_error_get_remote_error(error);
     const bool result = (remote_type && strcmp(type, remote_type) == 0);
-    g_free(remote_type);
     return result;
 }
 

src/gui-wizard-gtk/wizard.c
@@ -709,7 +709,7 @@ static void tv_details_row_activated(
         return;
 
     gint exitcode;
-    gchar *arg[3];
+    g_autofree gchar *arg[3];
     arg[0] = (char *) "xdg-open";
     arg[1] = g_build_filename(g_dump_dir_name ? g_dump_dir_name : "", item_name, NULL);
     arg[2] = NULL;
@@ -753,8 +753,6 @@ static void tv_details_row_activated(
         gtk_widget_destroy(scrolled);
         gtk_widget_destroy(dialog);
     }
-
-    free(arg[1]);
 }
 
 /* static gboolean tv_details_select_cursor_row(
@@ -1398,13 +1396,9 @@ static void update_command_run_log(const char* message, struct analyze_event_dat
         gtk_label_set_text(g_lbl_event_log, message);
 
     /* Don't append new line behind single dot */
-    const char *log_msg = it_is_a_dot ? message : g_strdup_printf("%s\n", message);
+    g_autofree const char *log_msg = it_is_a_dot ? message : g_strdup_printf("%s\n", message);
     append_to_textview(g_tv_event_log, log_msg);
     save_to_event_log(evd, log_msg);
-
-    /* Because of single dot, see lines above */
-    if (log_msg != message)
-        free((void *)log_msg);
 }
 
 static void run_event_gtk_error(const char *error_line, void *param)
@@ -2132,20 +2126,17 @@ static void search_item_to_list_store_item(GtkListStore *store, GtkTreeIter *new
         gtk_text_iter_backward_char(end);
     }
 
-    gchar *tmp = gtk_text_buffer_get_text(word->buffer, beg, &(word->start),
+    g_autofree gchar *tmp = gtk_text_buffer_get_text(word->buffer, beg, &(word->start),
             /*don't include hidden chars*/FALSE);
     g_autofree gchar *prefix = g_markup_escape_text(tmp, /*NULL terminated string*/-1);
-    g_free(tmp);
 
     tmp = gtk_text_buffer_get_text(word->buffer, &(word->start), &(word->end),
             /*don't include hidden chars*/FALSE);
     g_autofree gchar *text = g_markup_escape_text(tmp, /*NULL terminated string*/-1);
-    g_free(tmp);
 
     tmp = gtk_text_buffer_get_text(word->buffer, &(word->end), end,
             /*don't include hidden chars*/FALSE);
     g_autofree gchar *suffix = g_markup_escape_text(tmp, /*NULL terminated string*/-1);
-    g_free(tmp);
 
     char *content = g_strdup_printf("%s<span foreground=\"red\">%s</span>%s", prefix, text, suffix);
 
@@ -2324,13 +2315,10 @@ static gboolean highligh_words_in_tabs(GList *forbidden_words,  GList *allowed_w
 
 static gboolean highlight_forbidden(void)
 {
-    GList *forbidden_words = libreport_load_words_from_file(FORBIDDEN_WORDS_BLACKLLIST);
-    GList *allowed_words = libreport_load_words_from_file(FORBIDDEN_WORDS_WHITELIST);
+    g_autolist (GList) forbidden_words = libreport_load_words_from_file(FORBIDDEN_WORDS_BLACKLLIST);
+    g_autolist (GList) allowed_words = libreport_load_words_from_file(FORBIDDEN_WORDS_WHITELIST);
 
     const gboolean result = highligh_words_in_tabs(forbidden_words, allowed_words, /*case sensitive*/false);
-
-    libreport_list_free_with_free(forbidden_words);
-    libreport_list_free_with_free(allowed_words);
 
     return result;
 }
@@ -2649,7 +2637,7 @@ static gint select_next_page_no(gint current_page_no)
 
         log_info("%s: Looking for next event to process", __func__);
         /* (note: this frees and sets to NULL g_event_selected) */
-        char *event = setup_next_processed_event(&g_auto_event_list);
+        g_autofree char *event = setup_next_processed_event(&g_auto_event_list);
         if (!event)
         {
             current_page_no = PAGENO_EVENT_PROGRESS - 1;
@@ -2658,8 +2646,6 @@ static gint select_next_page_no(gint current_page_no)
 
         if (!get_sensitive_data_permission(event))
         {
-            free(event);
-
             cancel_processing(g_lbl_event_log, /* default message */ NULL, TERMINATE_NOFLAGS);
             current_page_no = PAGENO_EVENT_PROGRESS - 1;
             goto again;
@@ -2668,23 +2654,20 @@ static gint select_next_page_no(gint current_page_no)
         if (problem_data_get_content_or_NULL(g_cd, FILENAME_NOT_REPORTABLE))
         {
 
-            char *msg = g_strdup_printf(_("This problem should not be reported "
+            g_autofree char *msg = g_strdup_printf(_("This problem should not be reported "
                             "(it is likely a known problem). %s"),
                             problem_data_get_content_or_NULL(g_cd, FILENAME_NOT_REPORTABLE)
             );
 
             if (libreport_get_global_stop_on_not_reportable())
             {
-                free(event);
                 cancel_processing(g_lbl_event_log, msg, TERMINATE_NOFLAGS);
-                free(msg);
                 current_page_no = PAGENO_EVENT_PROGRESS - 1;
                 goto again;
             }
             else
             {
                 log_warning("%s", msg);
-                free(msg);
             }
         }
 
@@ -2737,11 +2720,9 @@ static gint select_next_page_no(gint current_page_no)
 
 static void rehighlight_forbidden_words(int page, GtkTextView *tev)
 {
-    GList *forbidden_words = libreport_load_words_from_file(FORBIDDEN_WORDS_BLACKLLIST);
-    GList *allowed_words = libreport_load_words_from_file(FORBIDDEN_WORDS_WHITELIST);
+    g_autolist (GList) forbidden_words = libreport_load_words_from_file(FORBIDDEN_WORDS_BLACKLLIST);
+    g_autolist (GList) allowed_words = libreport_load_words_from_file(FORBIDDEN_WORDS_WHITELIST);
     highligh_words_in_textview(page, tev, forbidden_words, allowed_words, /*case sensitive*/false);
-    libreport_list_free_with_free(forbidden_words);
-    libreport_list_free_with_free(allowed_words);
 }
 
 static void on_sensitive_word_selection_changed(GtkTreeSelection *sel, gpointer user_data)
@@ -2872,7 +2853,7 @@ static void on_btn_add_file(GtkButton *button)
             _("_Open"), GTK_RESPONSE_ACCEPT,
             NULL
     );
-    char *filename = NULL;
+    g_autofree char *filename = NULL;
     if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT)
         filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
     gtk_widget_destroy(dialog);
@@ -2883,7 +2864,7 @@ static void on_btn_add_file(GtkButton *button)
     {
         char *basename = strrchr(filename, '/');
         if (!basename)  /* wtf? (never happens) */
-            goto free_and_ret;
+            return;
         basename++;
 
         /* TODO: ask for the name to save it as? For now, just use basename */
@@ -2938,8 +2919,6 @@ static void on_btn_add_file(GtkButton *button)
             gtk_dialog_run(GTK_DIALOG(dlg));
             gtk_widget_destroy(dlg);
         }
- free_and_ret:
-        g_free(filename);
     }
 }
 
@@ -2972,9 +2951,8 @@ static void delete_item(GtkTreeView *treeview)
                     struct dump_dir *dd = wizard_open_directory_for_writing(g_dump_dir_name);
                     if (dd)
                     {
-                        char *filename = g_build_filename(g_dump_dir_name, item_name, NULL);
+                        g_autofree char *filename = g_build_filename(g_dump_dir_name, item_name, NULL);
                         unlink(filename);
-                        free(filename);
                         dd_close(dd);
                         g_hash_table_remove(g_cd, item_name);
                         gtk_list_store_remove(g_ls_details, &iter);

src/lib/abrt_xmlrpc.c
@@ -38,7 +38,7 @@ void abrt_xmlrpc_error(xmlrpc_env *env)
 
 struct abrt_xmlrpc *abrt_xmlrpc_new_client(const char *url, int ssl_verify)
 {
-    GList *proxies;
+    g_autolist (GList) proxies = NULL;
     xmlrpc_env env;
     xmlrpc_env_init(&env);
 
@@ -85,8 +85,6 @@ struct abrt_xmlrpc *abrt_xmlrpc_new_client(const char *url, int ssl_verify)
                          PACKAGE_NAME, VERSION,
                          &client_parms, XMLRPC_CPSIZE(transportparm_size),
                          &ax->ax_client);
-
-    libreport_list_free_with_free(proxies);
 
     if (env.fault_occurred)
         abrt_xmlrpc_die(&env);

commit 8fc14ba3e9aef7de2ce0d82fe9aba6b92b025180

rhbz: Fix a double-free condition
The `cc` string must not be freed after the variable goes out of scope
since it's appended to `cc_list`. (`g_list_append()` does not copy its
input.) We only need to free the last string in the loop, which is an
empty string.

The bug was introduced in 7aba6e53.

Resolves rhbz#1893595

src/plugins/rhbz.c
@@ -407,18 +407,20 @@ GList *rhbz_bug_cc(xmlrpc_value* result_xml)
         if (!item)
             continue;
 
-        const char* cc = NULL;
-        xmlrpc_read_string(&env, item, &cc);
+        char *cc = NULL;
+        xmlrpc_read_string(&env, item, (const char **)&cc);
         xmlrpc_DECREF(item);
         if (env.fault_occurred)
             abrt_xmlrpc_die(&env);
 
         if (*cc != '\0')
         {
-            cc_list = g_list_append(cc_list, (char*)cc);
+            cc_list = g_list_append(cc_list, cc);
             log_debug("member on cc is %s", cc);
             continue;
         }
+
+        free(cc);
     }
     xmlrpc_DECREF(cc_member);
     return cc_list;