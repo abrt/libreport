7342f26cb4d5ac8657a764295ff4f6540d516980
Michal Židek <mzidek@redhat.com>
2021-03-26 13:19:59 +0100

    cli: Address of local auto-variable assigned to a function parameter
    Error: CPPCHECK_WARNING (CWE-562): [#def7]
    libreport-2.14.0/src/cli/cli-report.c:771: error[autoVariables]: Address of local auto-variable assigned to a function parameter.

--- src/lib/run_event.c
+++ src/lib/run_event.c
@@ -51,6 +51,7 @@ void free_run_event_state(struct run_event_state *state)
     {
         g_ptr_array_free(state->extra_environment, TRUE);
         g_string_free(state->command_output, TRUE);
+        g_free(state->logging_param);
         free_commands(state);
         free(state);
     }
