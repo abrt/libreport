abrt/libreport

commit 7342f26cb4d5ac8657a764295ff4f6540d516980

cli: Address of local auto-variable assigned to a function parameter
Error: CPPCHECK_WARNING (CWE-562): [#def7]
libreport-2.14.0/src/cli/cli-report.c:771: error[autoVariables]: Address of local auto-variable assigned to a function parameter.

src/lib/run_event.c
@@ -51,6 +51,7 @@ void free_run_event_state(struct run_event_state *state)
     {
         g_ptr_array_free(state->extra_environment, TRUE);
         g_string_free(state->command_output, TRUE);
+        g_free(state->logging_param);
         free_commands(state);
         free(state);
     }

src/cli/cli-report.c
@@ -738,16 +738,16 @@ static int run_event_chain_real(struct run_event_state *run_state,
                                 GList                  *chain,
                                 int                     interactive)
 {
-    struct logging_state l_state;
+    struct logging_state *l_state = g_malloc0(sizeof(struct logging_state));
     int retval = 0;
 
     /* Blergh. */
     run_state->logging_callback = do_log2;
-    run_state->logging_param = &l_state;
+    run_state->logging_param = l_state;
 
     for (GList *eitem = chain; eitem; eitem = g_list_next(eitem))
     {
-        l_state.output_was_produced = false;
+        l_state->output_was_produced = false;
         const char *event_name = eitem->data;
         log_info("running commands for event '%s'", event_name);
 
@@ -770,7 +770,7 @@ static int run_event_chain_real(struct run_event_state *run_state,
                     event_name);
             continue;
         }
-        else if (retval != 0 || !l_state.output_was_produced)
+        else if (retval != 0 || !l_state->output_was_produced)
         {
             /* Program failed, or finished successfully with no output. */
             g_autofree char *msg = libreport_exit_status_as_string(event_name, run_state->process_status);

commit afb0a30238db8a68a7a610e6343a147dc235f50d

gtk: Fix segfault
A double free seems to have been introduced in 7342f26c.

Resolves https://bugzilla.redhat.com/show_bug.cgi?id=1965633

src/gui-wizard-gtk/wizard.c
@@ -1722,10 +1722,11 @@ static gboolean consume_cmd_output(GIOChannel *source, GIOCondition condition, g
         g_event_source_id = 0;
         close(evd->fd);
         g_io_channel_unref(evd->channel);
-        free_run_event_state(evd->run_state);
         g_string_free(evd->event_log, TRUE);
-        free(evd->event_name);
-        free(evd);
+        evd->event_log = NULL;
+        free_run_event_state(evd->run_state);
+        g_clear_pointer(&evd->event_name, g_free);
+        g_clear_pointer(&evd, g_free);
 
         /* Inform abrt-gui that it is a good idea to rescan the directory */
         kill(getppid(), SIGCHLD);

src/lib/run_event.c
@@ -51,7 +51,6 @@ void free_run_event_state(struct run_event_state *state)
     {
         g_ptr_array_free(state->extra_environment, TRUE);
         g_string_free(state->command_output, TRUE);
-        g_free(state->logging_param);
         free_commands(state);
         free(state);
     }